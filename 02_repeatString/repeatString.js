509b51f9c9af57d1d13d99758e80d757d4712238
dylansr99 <dylansr99@gmail.com>
2024-11-20 18:00:37 +0000

    Add loop to repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num){
+    let repeatedString = ''
+    if (num < 0) {repeatedString = 'ERROR'}
+    while (num > 0) {
+        repeatedString += string
+        num--
+    }
 
-};
+    return repeatedString
+}
+const number = Math.floor(Math.random() * 1000)
 
+repeatString('hi', 3)
+repeatString('hello', 10)
+repeatString('h1', 1)
+repeatString('hi', 0)
+repeatString('goodbye', -1)
+repeatString('odin', number)
+repeatString('', 10)
 // Do not edit below this line
 module.exports = repeatString;
+
