7340c025b25a096fd26285c3ebb686eee9334df7
dylansr99 <dylansr99@gmail.com>
2024-11-21 12:13:02 +0000

    Add code to reverse string using array methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+      let stringArray = string.split('')
+      let reversedArray = stringArray.toReversed()
+      return reversedArray.join('')
 };
 
+reverseString('hello')
 // Do not edit below this line
 module.exports = reverseString;
