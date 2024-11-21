aacde873dd07d24a5de50a2c80967c9d926a4d49
dylansr99 <dylansr99@gmail.com>
2024-11-21 12:15:20 +0000

    Call function for testing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -5,5 +5,8 @@ const reverseString = function(string) {
 };
 
 reverseString('hello')
+reverseString('hello there')
+reverseString('123! abc! Hello, Odinite.')
+reverseString('')
 // Do not edit below this line
 module.exports = reverseString;
