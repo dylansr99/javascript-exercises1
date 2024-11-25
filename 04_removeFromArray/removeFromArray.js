93caa7dc5b32d05ff777de9ff0ed9456ab14f425
dylansr99 <dylansr99@gmail.com>
2024-11-25 15:35:26 +0000

    Remove single element

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, value) {
+         let filteredArray = array.filter((arr) => arr != value)
+         return filteredArray
+     };                 
 
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
