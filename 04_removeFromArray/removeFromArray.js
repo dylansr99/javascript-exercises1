1444dff2f22737172b3262ae149f0431e57218ec
dylansr99 <dylansr99@gmail.com>
2024-12-02 19:11:29 +0000

    Change to use rest parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,12 @@
-const removeFromArray = function() {
-         let filteredArray = arguments[0].filter((arr) => arr != [arguments[1]])
-         return filteredArray
-     };                 
+const removeFromArray = function(array, ...args) {
+
+   return array.filter((arr) => arr != [args])
+   
+};                 
 
 removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2 );
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
