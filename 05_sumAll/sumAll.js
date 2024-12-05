734c47ec2f27d684aefe1fe7acba4446bb50cac8
dylansr99 <dylansr99@gmail.com>
2024-12-05 16:35:23 +0000

    Loop from firstValue to lastValue, push incremented value in to an array

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstValue, lastValue) {
+ 
+    let sumArray = []
+
+       for (i = firstValue; i <= lastValue; i++) {
+        
+       sumArray.push(i);
+
+       }
+
+    
 
 };
 
+sumAll(1, 4)
+
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// const initialValue = 0;
+    
+   //   const sumOfArray = sumArray.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue)
+
+     //   return sumOfArray
