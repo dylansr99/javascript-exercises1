84fe27d60d29ba9f9a65cd9813801f2a79192946
dylansr99 <dylansr99@gmail.com>
2024-12-08 17:13:21 +0000

    Add loop for firstValue being greater than lastValue

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,22 +1,31 @@
 const sumAll = function(firstValue, lastValue) {
- 
-    let sumArray = []
+// loops from firstValue to lastValue, pushing each increment in to sumArray
 
-       for (i = firstValue; i <= lastValue; i++) {
-        
-       sumArray.push(i);
+let sumArray = [];
+    
+if (firstValue > lastValue) {
+  for (i = lastValue; i <= firstValue; i++) {
+    sumArray.push(i)
+  } }
 
-       }
+  else if (firstValue < lastValue) {
+  for (i = firstValue; i <= lastValue; i++) {    
+    sumArray.push(i)    
+ }
+}
+
+          
+// accumulator starts at initialValue, current value in sumArray is added to accumulator until lastValue is reached. Returns whole number
 
     const initialValue = 0;
-    
       const sumOfArray = sumArray.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue)
-
         return sumOfArray
 
 };
 
 sumAll(1, 4)
+sumAll(1, 4000)
+sumAll(123, 1)
 
 // Do not edit below this line
 module.exports = sumAll;
