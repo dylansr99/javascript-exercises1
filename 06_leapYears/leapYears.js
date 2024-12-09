2049e2914204f23c1344749d52b7ef7ef7fbbde8
dylansr99 <dylansr99@gmail.com>
2024-12-09 16:35:38 +0000

    check if divisible by 4 and not divisible by 100

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+   if (year % 4 == 0 && year % 100 != 0) {return true}
 };
 
+leapYears(1996)
 // Do not edit below this line
 module.exports = leapYears;
