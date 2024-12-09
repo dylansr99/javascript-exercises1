08f0e1b15a4b32286c784649ed01cafc82c31d43
dylansr99 <dylansr99@gmail.com>
2024-12-09 19:21:51 +0000

    leapYears(34992)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,10 @@
 const leapYears = function(year) {
    if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0) {return true}
+     else {return false}
 };
 
 leapYears(1996)
+leapYears(1997)
+leapYears(34992)
 // Do not edit below this line
 module.exports = leapYears;
