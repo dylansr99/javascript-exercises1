168c8d52d263d8f3374be554f8ae544d5b8585d5
dylansr99 <dylansr99@gmail.com>
2024-12-14 16:05:08 +0000

    Add all function calls for tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -20,7 +20,8 @@ convertToCelsius(32)
 convertToCelsius(100)
 convertToCelsius(-100)
 convertToFahrenheit(0)
-
+convertToFahrenheit(73.2)
+convertToFahrenheit(-10)
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
