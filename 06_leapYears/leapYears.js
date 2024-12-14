42acbe740d0bd21cd008c60687f7e231349bb7a1
dylansr99 <dylansr99@gmail.com>
2024-12-14 15:34:23 +0000

    Change first if statement to only check for divisible by 4 and not divisible by 100, second if statement to check for divisible by 4 and 400

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,13 @@
 const leapYears = function(year) {
-   if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0) {return true}
-     else {return false}
+   if (year % 4 == 0 && year % 100 != 0) {return true}
+    else if (year % 4 == 0 && year % 400 == 0) {return true}
+      else {return false}
 };
 
 leapYears(1996)
 leapYears(1997)
 leapYears(34992)
+leapYears(1900)
+leapYears(1600)
 // Do not edit below this line
 module.exports = leapYears;
