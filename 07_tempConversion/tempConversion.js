a5336ed18f20deda46a95b7df685c41c3ab12d67
dylansr99 <dylansr99@gmail.com>
2024-12-14 15:55:32 +0000

    Convert from farenheit to celcius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    
+    return (temp - 32) / 1.8;
+
 };
 
-const convertToFahrenheit = function() {
+convertToCelsius(32)
+
+const convertToFahrenheit = function(temp) {
+
 };
 
 // Do not edit below this line
