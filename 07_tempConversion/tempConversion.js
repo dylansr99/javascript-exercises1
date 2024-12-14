00bd20c862d531b5bd345d6df040d2035e77a886
dylansr99 <dylansr99@gmail.com>
2024-12-14 16:03:21 +0000

    Convert from celcius to fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,19 +1,26 @@
 const convertToCelsius = function(temp) {
 
-  let convertedTemp = (temp - 32) / 1.8
+  let convertedTemp = (temp - 32) / 1.8;
     
   
-  return Number(convertedTemp.toFixed(1))
+  return Number(convertedTemp.toFixed(1));
 
 };
 
 const convertToFahrenheit = function(temp) {
+
+  let convertedTemp = temp * 1.8 + 32;
+
+  return Number(convertedTemp.toFixed(1))
 
 };
 
 
 convertToCelsius(32)
 convertToCelsius(100)
+convertToCelsius(-100)
+convertToFahrenheit(0)
+
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
