4effd2735f0c00e1b6a569af4849a45c69a6902c
dylansr99 <dylansr99@gmail.com>
2024-12-14 16:00:18 +0000

    Round to 1 decimal place, convert the string to a number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,19 @@
 const convertToCelsius = function(temp) {
+
+  let convertedTemp = (temp - 32) / 1.8
     
-    return (temp - 32) / 1.8;
+  
+  return Number(convertedTemp.toFixed(1))
 
 };
-
-convertToCelsius(32)
 
 const convertToFahrenheit = function(temp) {
 
 };
 
+
+convertToCelsius(32)
+convertToCelsius(100)
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
